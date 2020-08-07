35df7315c68fc8dd4f8ed85feffe012992f79512
moscalir <moscalir@uni.coventry.ac.uk>
2020-08-07 16:26:01 +0300

    add key

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search'
 import axios from 'axios';
 
 
@@ -15,7 +16,7 @@ class App extends Component {
    async componentDidMount() {
     // this is how to change a state in react using setState
    this.setState({loading: true})
-   const res= await axios.get('https://api.github.com/users');
+   const res= await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
    
    this.setState({users:res.data, loading:false})
   }
@@ -27,6 +28,7 @@ class App extends Component {
     <div className="App">
         <Navbar/>
         <div className="container">
+        <Search/>
         <Users loading={this.state.loading} users={this.state.users}/> 
         </div>
 
