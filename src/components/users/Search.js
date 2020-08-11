5afba7a48884e24dfc810d14a4fdb67cfb0463d1
moscalir <moscalir@uni.coventry.ac.uk>
2020-08-11 15:20:06 +0300

    converting the class search to a function by using useState

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,42 +1,29 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 
-export class Search extends Component {
+const Search =({searchUsers, showClear, clearUsers, setAlert}) => {
 
-    state = {
-        text: ""
-    }
+    const [text, setText] = useState("");
 
-    static propTypes = {
-        searchUsers: PropTypes.func.isRequired,
-        clearUsers: PropTypes.func.isRequired,
-        showClear: PropTypes.bool.isRequired,
-        setAlert: PropTypes.func.isRequired,
-    }
+    const onChange = (e) => setText(e.target.value);
 
-    onChange = (e) => this.setState({[e.target.name]: e.target.value});
-
-    onSubmit =(e)=>{
+    const onSubmit =(e)=>{
         e.preventDefault();
-        if(this.state.text ===""){
-            this.props.setAlert("Please enter something" , "light");
+        if(text ===""){
+            setAlert("Please enter something" , "light");
         }else {
             // pass the text to the main App.js
-            this.props.searchUsers(this.state.text);
-            this.setState({text:""})
+            searchUsers(text);
+            setText("");
 
         }
     }
 
-    render() {
-        //destructuring
-        const {showClear, clearUsers} =this.props
-
         return (
             <div>
-                <form onSubmit={this.onSubmit} className="form">
-                    <input type="text" name="text" placeholder="Search users..." value={this.state.text} onChange={this.onChange}
+                <form onSubmit={onSubmit} className="form">
+                    <input type="text" name="text" placeholder="Search users..." value={text} onChange={onChange}
                     />
                     <input type="submit" value="Search" className="btn btn-dark btn-block"></input>
                 </form>
@@ -47,9 +34,14 @@ export class Search extends Component {
             </div>
         )
     }
-}
 
 
+Search.propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
+};
 
-export default Search
- 
+
+export default Search;
